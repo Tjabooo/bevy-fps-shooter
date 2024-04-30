1114690a7f00827fac786dce36ba370d158dadb6
Tjabooo <39222357+Tjabooo@users.noreply.github.com>
2024-05-01 01:28:21 +0200

    Misc

--- src/modules/gunplay.rs
+++ src/modules/gunplay.rs
@@ -1,4 +1,4 @@
-use crate::rendering::entities::{EnemyController, GunController};
+use crate::rendering::entities::{EnemyController, GunController, PlayerController};
 use crate::game::CameraController;
 use bevy::prelude::*;
 use bevy_rapier3d::prelude::*;
@@ -11,37 +11,47 @@ pub fn setup(
 
 pub fn update(
     mut commands: Commands,
+    mut player_query: Query<(Entity, &Children), With<PlayerController>>,
+    mut camera_query: Query<(&GlobalTransform, &Children), (With<CameraController>, Without<PlayerController>)>,
     mut gun_query: Query<&GunController>,
-    mut camera_query: Query<&GlobalTransform, With<CameraController>>,
+    mut enemy_query: Query<&mut EnemyController>,
     rapier_context: Res<RapierContext>,
-    mut enemy_query: Query<&mut EnemyController>
 ) {
-    for gun_controller in gun_query.iter_mut() {
-        for camera_transform in camera_query.iter_mut() {
-            if gun_controller.shoot {
-                let bullet_ray = Ray3d {
-                    origin: camera_transform.translation(),
-                    direction: 
-                        Direction3d::new(
-                            Vec3::new(
+    if let Ok((player_entity, player_children)) = player_query.single() {
+        for child in player_children.iter() {
+            if let Ok((camera_transform, camera_children)) = camera_query.get(child) {
+                if let Ok(gun_controller) = gun_query.get(child) {
+                    if gun_controller.shoot {
+                        let bullet_ray = Ray3d {
+                            origin: camera_transform.translation(),
+                            direction: Direction3d::new(Vec3::new(
                                 camera_transform.forward().x,
                                 camera_transform.forward().y,
-                                camera_transform.forward().z
-                            )
-                        ).unwrap()
-                };
-                if let Some((entity, _toi)) = rapier_context.cast_ray(
-                    bullet_ray.origin,
-                    *bullet_ray.direction,
-                    100.0,
-                    true,
-                    QueryFilter::only_dynamic()
-                ) {
-                    println!("{:?}", entity);
+                                camera_transform.forward().z,
+                            ))
+                            .unwrap(),
+                        };
 
-                    if let Ok(mut enemy_controller) = enemy_query.get_mut(entity) {
-                        enemy_controller.health -= 1;
-                        println!("{:?}", enemy_controller.health);
+                        let filter = QueryFilter {
+                            flags: QueryFilterFlags::EXCLUDE_SENSORS | QueryFilterFlags::ONLY_DYNAMIC,
+                            exclude_collider: Some(player_entity),
+                            groups: None,
+                            ..Default::default()
+                        };
+
+                        if let Some((entity, _toi)) = rapier_context.cast_ray(
+                            bullet_ray.origin,
+                            *bullet_ray.direction,
+                            1000.0,
+                            true,
+                            filter,
+                        ) {
+                            println!("{:?}", entity);
+        
+                            if let Ok(mut enemy_controller) = enemy_query.get_mut(entity) {
+                                enemy_controller.health -= 1;
+                            }
+                        }
                     }
                 }
             }
